cdfa7b757bda2028fdc067d9eb09104e22b57ef0
Thower56 <112575794+Thower56@users.noreply.github.com>
2024-01-22 14:59:42 -0500

    init

---
+++ DSED-Module08_Exercice3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DSED_Module08_Exercice3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
